444f5f06c29f6d97f4b52753a051fc87859c9bde
duggank <CreativeWeep@gmail.com>
2014-12-04 10:11:37 -0500

    Initival Commit V2
    We did java

--- pfactor.java
+++ pfactor.java
@@ -3,29 +3,31 @@ public class pfactor
  
   public static void main(String[] args)
   {
-    int num = 20;
-    primeFactorization(num);
+    int num = 140;
+    System.out.print(primeFactorization(num));
     
-  }
-  
-  public static boolean isPrime(int num)
-  {
-       for(int i = num; i > 0; i--)
-       {
-         if(num % i == 0)
-         {
-           num /= i;
-           if(num != 1)
-           {
-             num /= i;
-           }
-         }      
-       }
   }
   
   
   public static String primeFactorization(int num)
-  { 
+  {
+      String out = "";
+      int thing;
+
+      int i = 2;
+      while(num > 1)
+      {
+          if(num % i == 0)
+          {
+              num /= i;
+              out += i + " x ";
+
+          }
+          else
+              i++;
+      }
+      return out;
+
   }
 
 }
