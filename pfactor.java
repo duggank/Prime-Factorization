d7e517eb7291a9551d70fbb379f4b25e2546767a
duggank <CreativeWeep@gmail.com>
2014-12-12 10:10:48 -0500

    Initial Commit V3
    ajkldfayhuoaighori

--- pfactor.java
+++ pfactor.java
@@ -3,31 +3,47 @@ public class pfactor
  
   public static void main(String[] args)
   {
-    int num = 140;
-    System.out.print(primeFactorization(num));
+    int num = 2;
     
+    while (num < 1000)
+    {
+    System.out.println(num + " = " + primeFactorization(num));
+    num++;
+    }
   }
   
   
   public static String primeFactorization(int num)
   {
       String out = "";
-      int thing;
 
       int i = 2;
       while(num > 1)
       {
           if(num % i == 0)
           {
+            if(num != i)  
+          {
+           
               num /= i;
               out += i + " x ";
+            }
+              else
+              {
+                num /= i;
+                out += num + " x " + i;
+              }
 
           }
-          else
+          
+          
               i++;
       }
+      if (out.substring(out.length() - 2).indexOf("x ") >= 0)
+      {
+        return out.substring(0, out.length() - 2);
+      }
       return out;
-
   }
 
 }
